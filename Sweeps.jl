ac2f119fe31ee9d417884e302b04abff98033f58
Ralf Rastlos <78442232+vollsynthetik@users.noreply.github.com>
2021-08-29 14:00:23 +0000

    added sweep types

--- Sweeps.jl
+++ Sweeps.jl
@@ -1,6 +1,9 @@
-export LogSweep, iterate, generatesample
+export Sweep, DiscreteSweep, DiscreteLogSweep, iterate, generatesample
 
-struct LogSweep
+abstract type Sweep end
+abstract type DiscreteSweep <: Sweep end
+
+struct DiscreteLogSweep <: DiscreteSweep
     duration::Number
     samplerate::Int
     lowestfrequency::Number
@@ -8,7 +11,7 @@ struct LogSweep
     amplitude::Number
 end
 
-function generatesample(sweep::LogSweep, number::Int)
+function generatesample(sweep::DiscreteLogSweep, number::Int)
     a0 = sweep.amplitude
     c = log(2, sweep.highestfrequency) - log(2, sweep.lowestfrequency)
     phi0 = -((sweep.lowestfrequency * sweep.duration) / (c * log(2)))
@@ -16,6 +19,6 @@ function generatesample(sweep::LogSweep, number::Int)
     Sample(a0 * sin(((sweep.lowestfrequency * sweep.duration) / (c * log(2))) * 2^(c*x / sweep.duration) + phi0))
 end
 
-function Base.iterate(S::LogSweep, state=1)
+function Base.iterate(S::DiscreteSweep, state=1)
     state > S.duration * S.samplerate ? nothing : (generatesample(S, state), state + 1)
 end
