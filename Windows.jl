f4bf092f510d33d803287d7b7aeab5e168ad5df5
Ralf Rastlos <78442232+vollsynthetik@users.noreply.github.com>
2021-08-30 16:30:31 +0000

    hann and hamming windowing functions added

--- Windows.jl
+++ Windows.jl
@@ -1,6 +1,6 @@
-export blackmanharris
+export applyblackmanharris
 
-function blackmanharris(samples::AbstractArray{Sample})::Array{Sample}
+function applyblackmanharris(samples::AbstractArray{Sample})::Array{Sample}
     window::Array{Sample} = []
     for index = 1:length(samples)
        push(window, blackmanharriswindow(samples[index].value, index, length(samples)))
@@ -14,4 +14,28 @@ function blackmanharriswindow(sample::Sample, index::Number, windowsize::Number)
                            0.14128*cos((4*pi*(index-1))/(windowsize-1)) - 
                            0.01168*cos((6*pi*(index-1))/(windowsize-1))
                            ))
+end
+
+function applyhann(samples::AbstractArray{Sample})::Array{Sample}
+    window::Array{Sample} = []
+    for index = 1:length(samples)
+       push(window, hannwindow(samples[index].value, index, length(samples)))
+    end
+    window
+end
+
+function hannwindow(sample::Sample, index::Number, windowsize::Number)::Sample
+    Sample(sample.value * (1/2) * (1 - cos((2*pi*(index-1))/(windowsize-1))))
+end
+
+function applyhamming(samples::AbstractArray{Sample})::Array{Sample}
+    window::Array{Sample} = []
+    for index = 1:length(samples)
+       push(window, hammingwindow(samples[index].value, index, length(samples)))
+    end
+    window
+end
+
+function hammingwindow(sample::Sample, index::Number, windowsize::Number)::Sample
+    Sample(sample.value * (0.54 - 0.46 * cos((2*pi*(index-1))/(windowsize-1))))
 end
