5ed08d65df6d0a623063043c943e43cf4d92e4d7
Ralf Rastlos <78442232+vollsynthetik@users.noreply.github.com>
2021-09-07 20:08:01 +0000

    windows: sample type is now generic

--- src/Windows.jl
+++ src/Windows.jl
@@ -1,41 +1,41 @@
-export applyblackmanharris
+export applyblackmanharris,applyhann,applyhamming
 
-function applyblackmanharris(samples::Array{Sample{Float64},1})::Array{Sample{Float64},1}
-    window::Array{Sample{Float64},1} = []
+function applyblackmanharris(samples::Array{Sample{T},1})::Array{Sample{T},1} where T <: Number
+    window::Array{Sample{T},1} = []
     for index = 1:length(samples)
        push(window, blackmanharriswindow(samples[index].value, index, length(samples)))
     end
     window
 end
 
-function blackmanharriswindow(sample::Sample{Float64}, index::Number, windowsize::Number)::Sample{Float64}
-    Sample{Float64}(sample.value * (0.35875 - 
+function blackmanharriswindow(sample::Sample{T}, index::Number, windowsize::Number)::Sample{T} where T <: Number
+    Sample{T}(sample.value * (0.35875 - 
                            0.48829*cos((2*pi*(index-1))/(windowsize-1)) - 
                            0.14128*cos((4*pi*(index-1))/(windowsize-1)) - 
                            0.01168*cos((6*pi*(index-1))/(windowsize-1))
                            ))
 end
 
-function applyhann(samples::Array{Sample{Float64}})::Array{Sample{Float64},1}
-    window::Array{Sample{Float64},1} = []
+function applyhann(samples::Array{Sample{T}})::Array{Sample{T},1} where T <: Number
+    window::Array{Sample{T},1} = []
     for index = 1:length(samples)
        push(window, hannwindow(samples[index].value, index, length(samples)))
     end
     window
 end
 
-function hannwindow(sample::Sample{Float64}, index::Number, windowsize::Number)::Sample{Float64}
-    Sample{Float64}(sample.value * (1/2) * (1 - cos((2*pi*(index-1))/(windowsize-1))))
+function hannwindow(sample::Sample{T}, index::Number, windowsize::Number)::Sample{T} where T <: Number
+    Sample{T}(sample.value * (1/2) * (1 - cos((2*pi*(index-1))/(windowsize-1))))
 end
 
-function applyhamming(samples::Array{Sample{Float64},1})::Array{Sample{Float64},1}
-    window::Array{Sample{Float64},1} = []
+function applyhamming(samples::Array{Sample{T},1})::Array{Sample{T},1} where T <: Number
+    window::Array{Sample{T},1} = []
     for index = 1:length(samples)
        push(window, hammingwindow(samples[index].value, index, length(samples)))
     end
     window
 end
 
-function hammingwindow(sample::Sample{Float64}, index::Number, windowsize::Number)::Sample{Float64}
-    Sample{Float64}(sample.value * (0.54 - 0.46 * cos((2*pi*(index-1))/(windowsize-1))))
+function hammingwindow(sample::Sample{T}, index::Number, windowsize::Number)::Sample{T} where T <: Number
+    Sample{T}(sample.value * (0.54 - 0.46 * cos((2*pi*(index-1))/(windowsize-1))))
 end
