1490a76ed4a6ff6f2be0cb3127f8c55d4d29886f
Ralf Rastlos <78442232+vollsynthetik@users.noreply.github.com>
2021-09-07 20:23:22 +0000

    added script for sweep fft generation + bug fixes

---
+++ scripts/logsweep.jl
@@ -0,0 +1,18 @@
+using FFTW
+using DelimitedFiles
+
+include("../src/RoomReverb.jl")
+
+using Main.RoomReverb
+
+samples = Array{Sample, 1}()
+
+samplingrate = 1024
+
+sweep = LogarithmicSweep(1, samplingrate, 100, 400, 1)
+samples = play(sweep)
+sweepfft = (v -> v / (samplingrate/2)).(abs.(fft(value.(samples))[2:512]))
+
+writedlm("ffts/logsweepfft.csv", sweepfft)
+
+println("Done.")
