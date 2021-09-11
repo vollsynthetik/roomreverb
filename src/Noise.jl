42c53e0b88569042b70f050706a031c9b9255db5
Ralf Rastlos <78442232+vollsynthetik@users.noreply.github.com>
2021-09-11 06:38:48 +0000

    pink noise implemented

--- src/Noise.jl
+++ src/Noise.jl
@@ -17,16 +17,19 @@ mutable struct WhiteNoise <: Noise
 end
 
 "Represents a discrete pink noise."
-struct PinkNoise <: Noise
+mutable struct PinkNoise <: Noise
     duration::Number
     samplerate::Int
     lowestfrequency::Number
     highestfrequency::Number
     amplitude::Number
+    buffer::Array{Sample{Float64}, 1}
+    PinkNoise(duration, samplerate, lowestfrequency, highestfrequency, amplitude) =
+        new(duration, samplerate, lowestfrequency, highestfrequency, amplitude, Array{Sample{Float64},1}())
 end
 
 "Represents a discrete brown noise."
-struct BrownNoise <: Noise
+mutable struct BrownNoise <: Noise
     duration::Number
     samplerate::Int
     lowestfrequency::Number
@@ -57,6 +60,24 @@ end
 
 "Gives a sample at a time for the given pink noise definition."
 function generatesample(noise::PinkNoise, number::Int)
+    if isempty(noise.buffer)
+        generatepinknoise(noise)
+    end
+    noise.buffer[number]
+end
+
+"Generates pink noise corresponding to the given pink noise definiton"
+function generatepinknoise(noise::PinkNoise)::Array{Sample{Float64}, 1}
+    fft = zeros(Complex{Float64}, noise.samplerate)
+    for frequency in noise.lowestfrequency:noise.highestfrequency
+        drop = 2^(log(2,noise.lowestfrequency)-log(2,frequency))
+        value = Complex{Float64}(noise.amplitude * drop * rand(1)[1] * (noise.samplerate/2), 0)
+        fft[frequency + 1] = value
+        fft[noise.samplerate - (frequency - 1)] = value
+    end
+    samples = (v -> Sample{Float64}(v)).(real.(ifft(fft)))
+    noise.buffer = samples
+    samples
 end
 
 "Gives a sample at a time for the given brown noise definition."
