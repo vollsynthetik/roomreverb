9c9f2f945c4dcf5a84b4de09653d0faef7d8bfe3
Ralf Rastlos <78442232+vollsynthetik@users.noreply.github.com>
2021-09-20 20:11:53 +0000

    sinusoidal interpolation: bug fix

--- src/Resampling.jl
+++ src/Resampling.jl
@@ -3,6 +3,8 @@ export resample2
 using FFTW
 
 struct SinusoidalInterpolation <: Sound
+    duration::Number
+    samplerate::Unsigned
     f::Function
 end
 
@@ -10,26 +12,30 @@ end
 function resample2(samples::AbstractVector{T} where T <: Real, sourcesamplerate::Integer, targetsamplerate::Integer)::Vector{Float64}
     sourceNyquist = div(sourcesamplerate, 2)
     return play(SinusoidalInterpolation(
+        length(samples) / sourcesamplerate,
+        targetsamplerate,
         generateinterpolationfunction(
             sourcesamplerate,
             targetsamplerate,
-            fft(samples)[1:sourceNyquist]
+            (s->s/sourceNyquist).(fft(samples)[1:sourceNyquist])
             )
         ))
 end
 
 function generateinterpolationfunction(sourcesamplerate::Integer, targetsamplerate::Integer, fbins::Vector{Complex{T}} where T <: Real)::Function
-    sinusoidscount = sourcesamplerate < targetsamplerate ? sourcesamplerate : targetsamplerate
-    return (t -> 
+    sourceNyquist = div(sourcesamplerate, 2)
+    targetNyquist = div(targetsamplerate, 2)
+    sinusoidscount = targetsamplerate < sourcesamplerate ? targetNyquist : sourceNyquist
+    return function (t::Number)
         sum = 0.
         for k in 0:sinusoidscount-1
-            sum += fbins[k] * exp(im 2 * pi * k * (t / targetsamplerate))
+            sum += fbins[k + 1] * exp(im * 2 * pi * k * ((t - 1) / targetsamplerate))
         end
-        sum / sinusoidscount
-        )
+        real(sum / sinusoidscount) * targetNyquist
+    end
 end
 
 "Gives a sample at a time for the given sinusoidal interpolation definition."
-function generatesample(interpolationFunction::InterpolationFunction, number::Integer)::Float64
-    interpolationFunction.f(number)
+function generatesample(interpolation::SinusoidalInterpolation, number::Integer)::Float64
+    interpolation.f(number)
 end
